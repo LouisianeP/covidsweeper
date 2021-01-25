c9029846da0cce35e4ce810e84bb563ff087d5ff
Louisiane_Berlin <corentine.piette@gmail.com>
2021-01-25 18:17:51 +0100

    connection with images

--- src/main.js
+++ src/main.js
@@ -1,26 +1,35 @@
 function loadBoard() {
+    let html =''
     shuffleBoard = shuffleBoard();
-    shuffleBoard.forEach((boardLine) => {
-        let i = 0
+    let i = 0
+    for (boardLine of shuffleBoard) {
     html += `<div class="line line${i}">`;
         for (boardElement of boardLine) {
     html += `<div class="boardElement">`;
-      html += `<div class="back"></div>`;
-      html += `<div class="front"><img src='./imgs/${boardElement}' width="15" height="15"></div>`;
-
-  
+    html += `<div class="back"></div>`;
+    html += `<div class="front"></div><img src='./imgs/${boardElement}.png' width="20" height="20">`;
+    html += `</div>`;
       }
       html += `</div>`;
-    });
-    memoryBoard.innerHTML = html;
-      .querySelectorAll(".boardElement")
-      .forEach((boardElement) => boardElement.addEventListener("click", revealValue()));
+      i+=1
+    };
+   // console.log(html)
+    board = document.querySelector('#board');
+    board.innerHTML = html;
+    console.log(board);
+      const allElements = document.querySelectorAll(".boardElement")
+      for (boardElement of allElements) {
+        boardElement.addEventListener("click", revealValue())  
+      } 
   }
+  //<img src='./imgs/${boardElement}' width="15" height="15">
+
+
 
 function shuffleBoard () {
     let matrixElements = 9*9;
     const bombs =[];
-    for (i=0; bombs.length<10; i++) {
+    for (i=0; bombs.length<11; i++) {
         bomb = Math.floor((Math.random()*matrixElements))
         if (bombs.indexOf(bomb)===-1) {
          let x = Math.floor(bomb/9);
@@ -32,16 +41,29 @@ function shuffleBoard () {
 
     const board = Array(9).fill().map(() => Array(9).fill(0));
 
-
+    console.log(bombs)
     for (bomb of bombs) {
         let x = bomb[0];
         let y = bomb[1];
         board[x][y]='X';
-        if ((x<8) && board[x+1][y] !=='X') board[x+1][y]+=1;
-        if ((y<8) && board[x][y+1] !=='X') board[x][y+1]+=1;
-        if ((x>0) && board[x-1][y] !=='X') board[x-1][y]+=1;
-        if ((y>0) && board[x][y-1] !=='X') board[x][y-1]+=1;
+        if ((x<8) && board[x+1][y] !=='X') {
+            board[x+1][y]+=1;
+            if(y>0 && board[x+1][y-1] !=='X') board[x+1][y-1]+=1;
+            if (y<8 && board[x+1][y+1] !=='X') board[x+1][y+1]+=1;
+        }
+        if ((x>0) && board[x-1][y] !=='X') {
+            board[x-1][y]+=1;
+            if(y>0 && board[x-1][y-1] !=='X') board[x-1][y-1]+=1;
+            if (y<8 && board[x-1][y+1] !=='X') board[x-1][y+1]+=1;
+        }
+        if ((y<8) && board[x][y+1] !=='X') {
+            board[x][y+1]+=1;
+        }
+        if ((y>0) && board[x][y-1] !=='X') {
+            board[x][y-1]+=1;
+        }
     }
+    console.log(board)
     return board;
 }
 
@@ -51,3 +73,4 @@ function revealValue() {
 
 
 shuffleBoard()
+loadBoard ()
